9ad827e89ef14e617c65ac8bc0a597dde7f11097
SDET1920 <ssshivamchauhan.com>
2026-05-11 01:01:41 +0530

    first code

---
+++ Day_1/Day_1_Variable.py
@@ -0,0 +1,14 @@
+# Variable-- Variable is container which can hold some data.
+
+#X is variable which is holding a value (100).
+
+x=100
+
+print(x)
+
+
+#Variable we can change any time like integer or string it's upto you.
+
+s="python"
+
+print(s)
