d76b90f7e3b588dab0e0b05fb45f62c561f264dc
SDET1920 <ssshivamchauhan.com>
2026-05-11 23:36:29 +0530

    Day2 data type added along with readme.md

---
+++ Day_2/Day_2_Data type.py
@@ -0,0 +1,44 @@
+# The data type dynamically changes in python automatically it's upto coder.
+
+
+## Example 1
+
+a=10
+b=20
+print(a+b)
+
+
+## Example 2
+
+a= 10
+b=20
+c="welcome"
+print(a,b,c)
+
+# Note string we can write  C='welcome', and c="welcome"
+## Example 3
+
+a='welcome to pyton world'
+b="Welcome to python to become SDET"
+print(a,b)
+
+## Example 4
+
+a,b,c=10,20,"SDET"
+print(a,b,c)
+
+
+## if value same
+## Exaple 5
+
+a=b=c=10
+print(a,b,c)
+
+## Swaping concept.
+
+x=1
+y=2
+print(x,y)
+
+y,x=x,y   ##swaping rule
+print(x,y)
---
+++ Day_2/README.md
@@ -0,0 +1,22 @@
+# Data Types
+
+1. Data type means the type of value stored in a variable, and it's dynamically changed automatically. 
+
+
+2. A data type defines the type of value a variable can store.
+
+Example:
+
+x = 10        # Integer
+name = "QA"   # String
+price = 10.5  # Float
+
+Common Python data types: Given below will use these data type in code practice.
+
+Integer (int)
+Float (float)
+String (str)
+Boolean (bool)
+List (list)
+Tuple (tuple)
+Dictionary (dict)
