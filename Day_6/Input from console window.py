c74ec904e446c4eb8b54a553b8207e0731b05117
SDET1920 <ssshivamchauhan.com>
2026-05-18 01:48:09 +0530

    Day_6 added with readme

---
+++ Day_6/Input from console window.py
@@ -0,0 +1,28 @@
+## How to take input from console.
+# How to take input from user & type conversion.
+## function type in conversion- input(), int(), float()
+
+#Example 1-
+num1=input("Enter first Number:")
+num2=input("Enter second Number:")
+
+print(num1+num2)  ##this will provide out as per concatenation.
+
+
+
+#Example 2-- int()
+#Step 1- Takes two numbers as input
+#Step 2- Converts them to integers using int()
+#Step 3- Prints their data types
+#Step 4- Prints their sum
+
+num1=int(input("Enter first Number:"))
+num2=int(input("Enter second Number:"))
+
+print(type(num1))  #it's for to print data type (int)
+print(type(num2))  #it's for to print data type (int)
+
+print(num1+num2)
+
+
+
---
+++ Day_6/README.md
@@ -0,0 +1,12 @@
+# How to take input from console.
+# How to take input from user & type conversion.
+
+## function type in conversion- input(), int(), float()
+
+In Python, you can take input from the console using the built-in input() function.
+If you want, I can also show:
+
+1- input in loops
+2- competitive programming style input
+3- taking list input
+4- input from files instead of console.
